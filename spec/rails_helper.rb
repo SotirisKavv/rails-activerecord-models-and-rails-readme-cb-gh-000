342ca768b8ac62cef643276759c57965c2c2eb7d
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-10-03 13:26:53 +0000

    Automatically backed up by Learn

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,10 @@ require 'rspec/rails'
 require "rails_helper"
 
 describe Post do
-
+  it 'can be created' do
+    post = Post.create!(title: "My Title", description: "The post description")
+    expect(post).to be_valid
+  end
 end
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
